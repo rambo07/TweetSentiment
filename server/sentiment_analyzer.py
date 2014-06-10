c59f5cd66350a67f0ca2287dfaecfa38ae70a21b
Chris Hokamp <chris.hokamp@gmail.com>
2014-06-10 17:52:27 +0100

    update sentiment analyzer

--- server/sentiment_analyzer.py
+++ server/sentiment_analyzer.py
@@ -11,10 +11,10 @@ from flask.ext.cors import cross_origin
 # the circular dependency is ok in this case
 from server import app
 
-# import ruleBased
+import ruleBased
 
-lines = [ (re.split(',', line)) for line in open('merge.csv').read().splitlines() ]
-lexicon = { word: float(score) for word, score in lines }
+# lines = [ (re.split(',', line)) for line in open('merge.csv').read().splitlines() ]
+# lexicon = { word: float(score) for word, score in lines }
 
 
 # The routes for the sentiment analyzer
@@ -24,6 +24,7 @@ def convert_morphology(lang):
     if lang == 'en':
         # get params from the request
         tweet = request.args['tweet']
-        return flask.jsonify({ tweet: tweet })
+        score = ruleBased.ruleScorer(tweet)
+        return flask.jsonify({ 'tweet': tweet, 'score': score })
     else:
         return flask.jsonify({ "error": "I don't know the language: " + lang })
