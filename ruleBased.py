1050b0874788a132df3c3eaa7af90e167f6ad470
piyush16 <piyush16@piyush16-Latitude-E5430-non-vPro.(none)>
2014-06-10 17:22:43 +0100

    second commit

--- ruleBased.py
+++ ruleBased.py
@@ -1,10 +1,17 @@
 import os;
 import sys;
 
+#Loading the Sentiment Lexicon in the dictionary
+lex={};
+f1=open("merge.csv").readlines();
+for i in f1:
+  temp = i.split(",");	
+  temp[1] = temp[1].strip("\n");
+  lex[temp[0].lower()]=temp[1];
 
-def calculateScore(sentence, lexicon):
+#Function for calculating tweet score
+def calculateScore(sentence):
   readLine = sentence.split();
-  lex = lexicon;
   pos_score =0.0;
   neg_score =0.0;
   for z in readLine:
@@ -19,58 +26,21 @@ def calculateScore(sentence, lexicon):
   window = 2;
   negativeWordsList = {'not':1,'no':1,'never':1,'n\'t':1}
   readLine = sentence.split();
-  try{
+  try:
      for i in range(index-window,index+window+1,1) 
 	  if(i!=index)
 	    if(negativeWordList.has_key(readLine[i].lower()))
               return 1; 		
-  }
-  except{
+  except:
 	continue;
-  }
   return 0;
 '''
  
 def main():
-  inputFile = sys.argv[1];
-  lexicon = sys.argv[2];
-  lex = {};
-  threshols = 0.0;
-  countPos=0;countNeg=0;countNeu=0;
-  countPosScore=0;countNegScore=0;countNeuScore=0;
+  sentence = "Hello Welcome to Brazil, time to set the stage";
+  score = calculateScore(sentence); 
+  print score;
   
-#Loading the lexicon file in a dictionary
-  f1=open(lexicon).readlines();
-  for i in f1:
-    temp = i.split(",");	
-    temp[1] = temp[1].strip("\n");
-    lex[temp[0].lower()]=temp[1];
-  
-#Reading tweets from a file sequentially
-  f2 = open(inputFile).readlines();
-  for j in f2:
-    sen=j.strip("\n");
-    print "Tweet-----",sen;
-    score = calculateScore(sen,lex);
-#    negCheck = checkForNegation(sen,j);
-    if(negCheck!=0)
-	score = -score;
-
-#Deciding the tweet polarity 
-    if( score > threshold):
-       countPos = countPos+1;	
-       countPosScore = countPosScore+score;	
-       print "Polarity-----Positive Tweet" 
-    elif( score < threshold):
-       countNeg = countNeg+1;	
-       countNegScore = countNegScore+score;	
-       print "Polarity-----Negative Tweet"
-    else:
-       countNeu = countNeu+1;	
-       countNeuScore = countNeuScore+score;	
-       print "Polarity-----Neutral"	 
-  print countPos, countNeg, countNeu;
-
 if __name__ == "__main__":
   main();
 
