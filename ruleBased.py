c2efd5072a67c578000a5158cb696e5d2c1aba1c
piyush16 <piyush16@piyush16-Latitude-E5430-non-vPro.(none)>
2014-06-11 10:41:19 +0100

    updated with Negation Handling

--- ruleBased.py
+++ ruleBased.py
@@ -12,36 +12,42 @@ for i in f1:
 #Function for calculating tweet score
 def calculateScore(sentence):
   readLine = sentence.split();
-  pos_score =0.0;
-  neg_score =0.0;
+  pos_score = 0.0;
+  neg_score = 0.0;
   for z in range(0,len(readLine),1):
     token = readLine[z].lower();
     if(lex.has_key(token)):
-#      checkForNegation(sentence,z)
+      negCheck = checkForNegation(sentence,z)
       if(lex[token] > 0.0):
-        pos_score = pos_score + float(lex[token]);
+	if(negCheck == 0):	
+          pos_score = pos_score + float(lex[token]);
+	else:
+          neg_score = pos_score + float(lex[token]);
       elif(lex[token] < 0.0):
-	neg_score = neg_score + float(lex[token]);
+	if(negCheck == 0):	
+	  neg_score = neg_score + float(lex[token]);
+	else:
+	  pos_score = neg_score + float(lex[token]);
   return (pos_score - neg_score); 
 
-'''def checkForNegation(sentence,index):
-  window = 2;
-  negativeWordsList = {'not':1,'no':1,'never':1,'n\'t':1}
+#Negation Handling
+def checkForNegation(sentence,index):
+  window = 2;	#Window for looking for Negators-> negativity indicators
+  negativeWordsList = {'not':'1','no':'1','never':'1','n\'t':'1'}
   readLine = sentence.split();
   try:
-     for i in range(index-window,index+window+1,1): 
-	  if(i!=index):
-	    if(negativeWordList.has_key(readLine[i].lower())):
-              return 1; 		
+    for i in range(index-window,index+window+1,1):   
+      if(negativeWordsList.has_key(readLine[i].lower())):
+        return 1; 	
   except:
-	continue;
+    1; 
   return 0;
-'''
+
  
 def ruleScorer(sentence):
   score = calculateScore(sentence); 
   return score;
-  
+
 if __name__ == "__main__":
   main();
 
