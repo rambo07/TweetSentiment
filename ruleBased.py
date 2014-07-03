71f6d1e8521615afde24be35a9e0b1a1c777e0bc
Chris Hokamp <chris.hokamp@gmail.com>
2014-07-03 14:47:42 +0100

    update styles and fix negative bug

--- ruleBased.py
+++ ruleBased.py
@@ -31,50 +31,50 @@ def calculateScore(sentence):
   epos_score = 0.0; eneg_score = 0.0;
   cpos = 0; cneg = 0; cneu=0;
   try:
-    for z in range(0,len(readLine),1):
-      token = readLine[z].lower();
-      if(lex.has_key(token)):
+    for z,tok in enumerate(readLine):
+      tok = tok.lower();
+      if(lex.has_key(tok)):
         lex_match += 1	
         negCheck = checkForNegation(sentence,z)
-        if(float(lex[token]) > 0.0):
-	  if(negCheck == 0):	
-            pos_score = pos_score + float(lex[token]);
-	    cpos = cpos+1;
-	  else:
-            neg_score = pos_score + float(lex[token]);
-        elif(float(lex[token]) < 0.0):
-	  if(negCheck == 0):	
-	    cneg = cneg+1;
-	    neg_score = neg_score + float(lex[token]);
-	  else:
-	    pos_score = neg_score + float(lex[token]);
-      elif(elex.has_key(token)):
-        elex_match += 1	
-        if(elex[token] > 0.0):
-          epos_score = epos_score + float(elex[token]);
-        elif(elex[token] > 0.0):
-          eneg_score = eneg_score + float(elex[token]);
-    if(elex_match!=0):
-	emoticon_score=((epos_score-eneg_score)/elex_match);
-    if(lex_match!=0):	
-        lexical_score = ((pos_score-neg_score)/lex_match)/4.0
+        if float(lex[tok]) > 0.0:
+            if negCheck == 0:
+                pos_score = pos_score + float(lex[tok])
+                cpos = cpos+1
+            else:
+                neg_score = pos_score + float(lex[tok])
+        elif float(lex[tok]) < 0.0:
+            if negCheck == 0:
+                cneg = cneg+1;
+                neg_score = neg_score + float(lex[tok])
+            else:
+                pos_score = neg_score + float(lex[tok])
+        elif elex.has_key(tok):
+            elex_match += 1
+            if elex[tok] > 0.0:
+                epos_score = epos_score + float(elex[tok])
+            elif elex[tok] < 0.0:
+                eneg_score = eneg_score + float(elex[tok]);
+        if elex_match!=0:
+            emoticon_score = ((epos_score-eneg_score)/elex_match)
+        if lex_match!=0:
+            lexical_score = ((pos_score+neg_score)/lex_match)/4.0
 
-    ans=emoticon_score+lexical_score;	
-    if(elex_match!=0 and lex_match!=0):	
-	ans = ans/2.0
-    return ans;	
-
-  except:
-    return 0;	
+    ans=emoticon_score+lexical_score
+    if elex_match != 0 and lex_match != 0:
+        ans = ans/2.0
+    return ans
+  except KeyError:
+    return 0
 
 #Negation Handling
 def checkForNegation(sentence,index):
-  window = 2;	#Window for looking for Negators-> negativity indicators
+  #Window for looking for Negators-> negativity indicators
+  window = 2
   negativeWordsList = {'not':'1','no':'1','never':'1','n\'t':'1'}
-  readLine = sentence.split();
+  readLine = sentence.split()
   try:
-    for i in range(index-window,index+window+1,1):   
-      if(negativeWordsList.has_key(readLine[i].lower())):
+    for i in range(index-window,index+window+1, 1):
+      if negativeWordsList.has_key(readLine[i].lower()):
         return 1; 	
   except:
     pass;
@@ -85,8 +85,8 @@ def ruleScorer(sentence):
   score = calculateScore(sentence); 
   return score; #score range{-1,1}
 
-if __name__ == "__main__":
-  main();
+# if __name__ == "__main__":
+#   main();
 
    	
 		
